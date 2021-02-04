3e4368159bd9440262a260e4e12f4febfcdfabcd
Lambourl <65184664+Lambourl@users.noreply.github.com>
2021-02-04 16:10:53 +0100

    Revert ":wrench: without git2/sys/features.h"
    This reverts commit 35c5957e99e21a4e3461d72b4e35ad78e1087447.

--- src/unix/posix.h
+++ src/unix/posix.h
@@ -7,9 +7,9 @@
 #ifndef INCLUDE_unix_posix_h__
 #define INCLUDE_unix_posix_h__
 
-//#ifndef LIBGIT2_NO_FEATURES_H
-//# include "git2/sys/features.h"
-//#endif
+#ifndef LIBGIT2_NO_FEATURES_H
+# include "git2/sys/features.h"
+#endif
 
 #include <stdio.h>
 #include <dirent.h>
